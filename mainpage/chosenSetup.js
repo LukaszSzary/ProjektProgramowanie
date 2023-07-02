d9d3cb706b528660e533c551288d65abd6af3332
Bartosz Machniak <b.machniak01@gmail.com>
2023-07-02 19:32:27 +0200

    nowa podstrona

--- mainpage/chosenSetup.js
+++ mainpage/chosenSetup.js
@@ -4,3 +4,17 @@ $(".chosen-select").chosen({
     no_results_text: "Empty",
 
 }); 
+
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 300,
+      values: [ 20, 100 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val(ui.values[ 0 ] + "zł - " + ui.values[ 1 ] + "zł");
+      }
+    });
+    $( "#amount" ).val($( "#slider-range" ).slider( "values", 0 ) + 
+      "zł - " + $( "#slider-range" ).slider( "values", 1 ) + "zł" );
+  } );
